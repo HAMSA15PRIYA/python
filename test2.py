082e58e029d9d13a7d5c56dccd6b279fe1ad0fee
HAMSA15PRIYA <54092362+HAMSA15PRIYA@users.noreply.github.com>
2019-08-14 15:24:04 +0530

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,13 @@
+word=input()
+if (word == 'a'):
+    print("Vowel")
+elif (word == 'e'):
+    print("Vowel")
+elif (word == 'i'):
+    print("Vowel")
+elif (word == 'o'):
+    print("Vowel")
+elif (word == 'u'):
+    print("Vowel")
+else:
+    print("Consonant")
